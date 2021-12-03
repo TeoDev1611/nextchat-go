c8e46e1d4fad1b77a577296c0bef275c0b05ac1c
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-12-02 21:02:00 -0500

    feat: improve the util for error managment

---
+++ utils/errors.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/labstack/gommon/log"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		data := fmt.Sprintf("ERROR DETECTED: %s", err.Error())
+		log.Error(data)
+	} else {
+		return
+	}
+}
